8843238874d319507b1bd66ac821de669da45169
AldroX <rhysandmalone@gmail.com>
2025-10-14 22:44:18 -0400

    fix: Refact task-card (method delete)

--- src/app/features/Task_Manager/task-card/task-card.component.ts
+++ src/app/features/Task_Manager/task-card/task-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Tareas } from '@core/models/Tareas.models';
 import { MatCardModule } from '@angular/material/card';
@@ -6,6 +6,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardContent } from '@angular/material/card';
 import { MatCardActions } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { DialogService } from 'app/core/services/dialog.service';
+import { TaskService } from 'app/core/services/task.service';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-task-card',
@@ -17,10 +20,40 @@ import { MatIconModule } from '@angular/material/icon';
     MatCardContent,
     MatCardActions,
     MatIconModule,
-  ],
+    RouterLink
+],
   templateUrl: './task-card.component.html',
   styleUrl: './task-card.component.scss',
 })
 export class TaskCardComponent {
   @Input() task!: Tareas;
+  @Output() editTask = new EventEmitter<Tareas>();
+  @Output() deleteTask = new EventEmitter<Tareas>();
+
+  constructor(
+    private dialogService: DialogService,
+    private taskService: TaskService
+  ) {}
+
+  onViewTask(): void {
+    // Implementar vista de detalles si es necesario
+    console.log('Ver tarea:', this.task);
+  }
+
+  onEditTask(): void {
+    this.editTask.emit(this.task);
+  }
+
+  onDeleteTask(): void {
+    this.dialogService
+      .openConfirmDialog(
+        'Eliminar Tarea',
+        '¿Estás seguro de que quieres eliminar esta tarea?'
+      )
+      .subscribe((confirmed) => {
+        if (confirmed) {
+          this.deleteTask.emit(this.task);
+        }
+      });
+  }
 }
