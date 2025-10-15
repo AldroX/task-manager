85642a537564988ee8ebf25eef900b6f3767bf29
AldroX <rhysandmalone@gmail.com>
2025-10-14 22:40:32 -0400

    feat: Refact routes and add create and update rutes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,8 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./layout/layout.component').then(c => c.LayoutComponent),
+    loadComponent: () =>
+      import('./layout/layout.component').then((c) => c.LayoutComponent),
     children: [
       {
         path: '',
@@ -12,11 +13,31 @@ export const routes: Routes = [
       },
       {
         path: 'dashboard',
-        loadComponent: () => import('./feature-manager-task/dashboard.page').then(c => c.DashboardPageComponent),
+        loadComponent: () =>
+          import('./feature-manager-task/dashboard.page').then(
+            (c) => c.DashboardPageComponent
+          ),
       },
       {
         path: 'tasks',
-        loadComponent: () => import('./features/Task_Manager/list-task/list-task.component').then(c => c.ListTaskComponent),
+        loadComponent: () =>
+          import('./features/Task_Manager/list-task/list-task.component').then(
+            (c) => c.ListTaskComponent
+          ),
+      },
+      {
+        path: 'tasks/create',
+        loadComponent: () =>
+          import(
+            './features/Task_Manager/create-update-form/create-update-form.component'
+          ).then((c) => c.CreateUpdateFormComponent),
+      },
+      {
+        path: 'tasks/edit/:id',
+        loadComponent: () =>
+          import(
+            './features/Task_Manager/create-update-form/create-update-form.component'
+          ).then((c) => c.CreateUpdateFormComponent),
       },
     ],
   },
