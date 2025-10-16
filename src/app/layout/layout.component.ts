c1fb3bdf05b9bad17480f0f9cc3367160872821c
AldroX <rhysandmalone@gmail.com>
2025-10-15 23:52:13 -0400

    feat: se actualizo el layout con el nuevo servicio de sidebar

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,15 +1,17 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
 import { MainComponent } from './main/main.component';
+import { SidebarService } from 'app/core/services/sidebar.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [HeaderComponent, SideBarComponent, MainComponent],
+  imports: [HeaderComponent, SideBarComponent, MainComponent, AsyncPipe],
   template: `
 <app-header></app-header>
-<div class="layout-container">
+<div class="layout-container" [class.collapsed]="isCollapsed$ | async">
   <app-side-bar></app-side-bar>
   <app-main></app-main>
 </div>
@@ -17,4 +19,7 @@ import { MainComponent } from './main/main.component';
   styleUrl: './layout.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LayoutComponent { }
+export class LayoutComponent {
+  sideBarService = inject(SidebarService);
+  isCollapsed$ = this.sideBarService.isCollapsed$;
+ }
